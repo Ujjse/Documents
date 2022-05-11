cf146557747b26bae701a0d5b786c813462e8fb0
William Lim <ujjwalpariyar@Ujjwals-MacBook-Pro.local>
2022-05-11 08:00:10 +0545

    First commit

---
+++ hw1.py
@@ -0,0 +1,10 @@
+print(" Choose the category of license you want.\n 1. A for scooter. \n 2. B for Bike. \n 3. C for Cars")
+category=input("Enter the catagory of trail you want to give: ")
+if category=="A":
+  print("You have trail of Scooter")
+elif category=="B":
+  print("You have trail of Bike")
+elif category=="C":
+  print("You have trail of Cars")
+else:
+  print("Invalid")
